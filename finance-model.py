870aef9a0ff1a0e4f39c60a56e37ec71aadada4c
sagniho <79772266+sagniho@users.noreply.github.com>
2024-10-03 12:11:07 -0400

    Update finance-model.py

--- finance-model.py
+++ finance-model.py
@@ -493,13 +493,15 @@ def main():
     user_username = st.secrets["auth"]["user_username"]
     user_password = st.secrets["auth"]["user_password"]
 
+
+
+    
     # User Authentication
     names = ['Admin User', 'C&I User']
     usernames = [admin_username, user_username]
     passwords = [admin_password, user_password]
-
     hashed_passwords = stauth.Hasher.hash_passwords(passwords)
-
+    
     credentials = {
         'usernames': {
             admin_username: {
@@ -512,6 +514,7 @@ def main():
             }
         }
     }
+
 
     authenticator = stauth.Authenticate(credentials, 'some_cookie_name', 'some_signature_key', cookie_expiry_days=30)
 
